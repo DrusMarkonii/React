a4550828708a91b0e906ba75fb13f06f1536011b
DrusMarkonii <andrey8566ok@gmail.com>
2021-08-25 22:21:27 +0300

    first app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserInfo userName="Вова" 
+        userGender="муж" 
+        userAge="27" 
+        yearsWorked="1" />
+      <br />
+      <UserInfo 
+        userName="Иван" 
+        userGender="муж" 
+        userAge="21" 
+        yearsWorked="2" />
+      <br />
+      <UserInfo
+        userName="Ольга"
+        userGender="жен"
+        userAge="37"
+        yearsWorked="3"
+      />
+    </div>
+  );
+  // <div>
+  //   <SayHi user="Jone" a="1" b="123"/>
+  //   <SayHi user="Drus" a="10" b="23123"/>
+  // </div>
+}
+
+// function SayHi(props) {
+//   console.log(props)
+//   return (
+//   <div>
+//     Hi, {props.user}
+//   </div>
+//   )
+// }
+
+function UserInfo(props) {
+  console.log(props);
+  return (
+    <div>
+      Имя: {props.userName} <br />
+      Пол: {props.userGender} <br />
+      Возраст: {props.userAge} <br />
+      Работает: {props.yearsWorked} <br />
+      ___________________
+      <br />
+      Зарплата: {props.yearsWorked * 10}
     </div>
   );
 }
