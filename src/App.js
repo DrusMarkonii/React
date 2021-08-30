14a918ae817881a640325e07fd48e0da03756264
DrusMarkonii <andrey8566ok@gmail.com>
2021-08-30 22:30:53 +0300

    feat: add scss

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 import "./App.css";
+import "./scss/app.scss"
 
 function App() {
   return (
-    <div className="container ">
-      <ul className="user-list">
-        <li className="user-item">
-          <UserInfo 
-            userName="Вова" 
-            userGender="муж" 
-            userAge={27} 
-            yearsWorked={1} 
-          />
-        </li>
-        <li className="user-item">
-          <UserInfo 
-            userName="Иван" 
-            userGender="муж" 
-            userAge={21} 
-            yearsWorked={2} 
-          />
-        </li>
-        <li className="user-item">
-          <UserInfo
-            userName="Ольга"
-            userGender="жен"
-            userAge={37}
-            yearsWorked={3}
-          />
-        </li>
-      </ul>
-      
-      
-      
-      
+    <div className="container">
+      <span>
+         hello my friend
+      </span>
       
     </div>
-  );
+    
+  )
 }
+    
 
-function UserInfo(props) {
-  console.log(props);
-  return (
-    <div>
-      Имя: {props.userName} <br />
-      Пол: {props.userGender} <br />
-      Возраст: {props.userAge} <br />
-      Работает: {props.yearsWorked} <br />
-      ___________________
-      <br />
-      Зарплата: {props.yearsWorked * 10}
-    </div>
-  );
-}
 
 export default App;
