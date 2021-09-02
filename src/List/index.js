ceb6d533536d4c5b41dd789f08b58358d1a458c1
DrusMarkonii <andrey8566ok@gmail.com>
2021-09-02 23:19:31 +0300

    feat: add id

--- src/List/index.js
+++ src/List/index.js
@@ -2,50 +2,55 @@ import React from 'react';
 import PersonDescription from './person-description';
 
 const people = [
-    {
+    {   id: 1,
         name: 'Дима',
         job:'Спикер',
         salary: 1
     },
     {
+        id: 2,
         name: 'Вова',
         job:'Тим-лид',
         salary: 2000
     },
     {
+        id: 3,
         name: 'Андрей',
         job:'Фрон-энд разработчик',
         salary: 1500
     },
     {
+        id: 4,
         name: 'Даниил',
         job:'Верстальщик',
         salary: 1000
     }
 ]
 
-function ListNotGoodButNotBad() {
-    let list = []
-    for(let i = 0; i < people.length; i++){
-        list.push(
-            <PersonDescription 
-                person={people[i]}
-            />
-        )
-    }
-    const heading = (
-        <h2>
-        Информация о сотрудниках:
-        </h2>
-    )
+// function ListNotGoodButNotBad() {
+//     let list = []
+//     for(let i = 0; i < people.length; i++){
+//         list.push(
+//             <PersonDescription 
+//                 person={people[i]}
+//             />
+//         )
+//     }
+//     const heading = (
+//         <h2>
+//         Информация о сотрудниках:
+//         </h2>
+//     )
         
-       return (
-       <div>
-            {heading}
-            {list}
-       </div>
-    )
-}
+//        return (
+//        <div>
+//             {heading}
+//             {list}
+//        </div>
+//     )
+// }
+
+
 
 function List () {
     return (
@@ -53,15 +58,16 @@ function List () {
              <h2>
                  Информация о сотрудниках:
             </h2>
-            {
-                people.map((item, index)=>{
-                    return (
-                        <PersonDescription 
-                            person={item}
-                        />
-                    )
-                })
-            }
+            {people.map((item, index)=>{
+                return (
+                    <PersonDescription 
+                        person={item}
+                        key={item.id}
+                    />
+                )
+            })}
+            
+            
         </div>
        
         
