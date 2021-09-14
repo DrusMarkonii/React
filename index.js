a8a35d4c2b3ab2168524fb1b5c02a1077ce188ec
DrusMarkonii <andrey8566ok@gmail.com>
2021-09-15 00:02:11 +0300

    feat: add form

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const btn = document.querySelector(".btn");
+const btn = document.querySelector(".btn-hello");
+
+document.forms.publish.onsubmit = function () {
+  var message = this.message.value;
+  alert(message);
+  return false;
+};
 
 const hendler = function () {
-  console.log("hello");
+  alert("hello");
 };
 
 btn.addEventListener("click", hendler);
